dc06408e7fec3482bc1aba991b2df56c27ef62e4
Celine Diks <cdiks@f0r2s7.codam.nl>
2022-05-02 10:27:10 +0200

    check_quotes function

--- is_check.c
+++ is_check.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   is_check.c                                         :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: rkoper <rkoper@student.codam.nl>             +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2022/04/26 14:54:34 by rkoper        #+#    #+#                 */
-/*   Updated: 2022/04/26 14:58:09 by rkoper        ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   is_check.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdiks <cdiks@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/26 14:54:34 by rkoper            #+#    #+#             */
+/*   Updated: 2022/05/02 10:11:46 by cdiks            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,32 @@ int	iswhitespace(char c)
 int istoken(char c)
 {
 	return (c == 60 || c == 62 || c == 39 || c == 34 || c == 124);
+}
+
+int check_quotes(char *line)
+{
+	int	i;
+	int	s_quotes;
+	int	d_quotes;
+
+	s_quotes = 0;
+	d_quotes = 0;
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == 34)
+		{
+			d_quotes++;
+			i++;
+		}
+		else if (line[i] == 39)
+		{
+			s_quotes++;
+			i++;
+		}
+		i++;
+	}
+	if ((s_quotes % 2) != 0 || (d_quotes % 2 != 0))
+		printf("No closing quote\n");
+	return (0);
 }
